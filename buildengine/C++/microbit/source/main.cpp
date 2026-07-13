263f5b2264b62006972703e6d1269089295d486b
Akhilesh G <akhilesh.gollapudi@outlook.com>
2026-07-13 09:01:24 -0600

    BIG UPDAT

--- buildengine/C++/microbit/source/main.cpp
+++ buildengine/C++/microbit/source/main.cpp
@@ -2,14 +2,13 @@
 
 MicroBit uBit;
 
+void onButtonA(MicroBitEvent) {
+    uBit.display.scroll("A");
+}
+
 int main() {
     uBit.init();
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
 
-    while (true) {
-        int x = uBit.accelerometer.getX();
-        int y = uBit.accelerometer.getY();
-
-        uBit.display.scroll("(" + ManagedString(x) + "," + ManagedString(y) + ")");
-        uBit.sleep(200);
-    }
+    release_fiber();
 }
